81e79465d4143828d441b707be2b6ea58322d4bf
progriro <progriro@gmail.com>
2021-11-25 01:24:09 +0900

    [add] useLoaderData and loader function to fetch slug data

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,7 +1,16 @@
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  return params.slug;
+};
+
 export default function PostSlug() {
+  const slug = useLoaderData();
+
   return (
     <div>
-      <h1>Some Post</h1>
+      <h1>Some Post: {slug}</h1>
     </div>
   );
 }
