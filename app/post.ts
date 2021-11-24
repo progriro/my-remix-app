983ce57c3b559c73423685fbf87f03ea78deb1e4
progriro <progriro@gmail.com>
2021-11-25 01:19:11 +0900

    [add] isValidPostAttributes of type check by tiny-invariant

--- app/post.ts
+++ app/post.ts
@@ -1,14 +1,25 @@
 import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
+import invariant from "tiny-invariant";
 
 export type Post = {
   slug: string;
   title: string;
 };
 
+export type PostMarkdownAttributes = {
+  title: string;
+};
+
 // relative to the server output not the source!
 const postsPath = path.join(__dirname, "..", "posts");
+
+function isValidPostAttributes(
+  attributes: any
+): attributes is PostMarkdownAttributes {
+  return attributes?.title;
+}
 
 export async function getPosts() {
   const dir = await fs.readdir(postsPath);
@@ -16,6 +27,10 @@ export async function getPosts() {
     dir.map(async (filename) => {
       const file = await fs.readFile(path.join(postsPath, filename));
       const { attributes } = parseFrontMatter(file.toString());
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`
+      );
       return {
         slug: filename.replace(/\.md$/, ""),
         title: attributes.title,
