a302c244b680a1107fe2ffa5d98d83753d2c1bba
progriro <progriro@gmail.com>
2021-11-25 01:28:14 +0900

    [feat] fetch post data via getPost function in slug page

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,16 +1,22 @@
 import { useLoaderData } from "remix";
 import type { LoaderFunction } from "remix";
+import { getPost } from "~/post";
+import type { Post } from "~/post";
+import invariant from "tiny-invariant";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  return params.slug;
+  // post データを js としてブラウザに含めるのではなく、データソースから取得していることに注目
+  // ファイル名を invariant で検証して型安全にすることは良い習慣
+  invariant(params.slug, "expected params.slug");
+  return getPost(params.slug);
 };
 
 export default function PostSlug() {
-  const slug = useLoaderData();
+  const post = useLoaderData<Post>();
 
   return (
     <div>
-      <h1>Some Post: {slug}</h1>
+      <h1>{post.title}</h1>
     </div>
   );
 }
