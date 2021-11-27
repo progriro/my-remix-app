a9ea42b446791eb0490a10a8fbcc30b8e507d543
progriro <progriro@gmail.com>
2021-11-27 15:45:20 +0900

    [feat] create post logic by using useActionData

--- app/post.ts
+++ app/post.ts
@@ -4,6 +4,12 @@ import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
 import { marked } from "marked";
 
+export type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+};
+
 export type Post = {
   slug: string;
   html: string;
@@ -51,4 +57,10 @@ export async function getPost(slug: string) {
   );
   const html = marked(body);
   return { slug, html, title: attributes.title };
+}
+
+export async function createPost(post: NewPost) {
+  const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  await fs.writeFile(path.join(postsPath, post.slug + ".md"), md);
+  return getPost(post.slug);
 }
--- app/routes/admin/new.tsx
+++ app/routes/admin/new.tsx
@@ -1,20 +1,53 @@
-import { Form } from "remix";
+import { useActionData, redirect, Form } from "remix";
+import type { ActionFunction } from "remix";
+import { createPost } from "~/post";
+import invariant from "tiny-invariant";
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: !title,
+    slug: !slug,
+    markdown: !markdown,
+  };
+
+  if (Object.values(errors).some((e) => e === true)) {
+    return errors;
+  }
+
+  invariant(typeof title === "string");
+  invariant(typeof slug === "string");
+  invariant(typeof markdown === "string");
+  await createPost({ title, slug, markdown });
+
+  return redirect("/admin");
+};
 
 export default function NewPost() {
+  const errors = useActionData();
+
   return (
     <Form method="post">
       <p>
         <label>
-          Post Title: <input type="text" name="title" />
+          Post Title: {errors?.title && <em>Title is required</em>}
+          <input type="text" name="title" />
         </label>
       </p>
       <p>
         <label>
-          Post Slug: <input type="text" name="slug" />
+          Post Slug: {errors?.slug && <em>Slug is required</em>}
+          <input type="text" name="slug" />
         </label>
       </p>
       <p>
-        <label htmlFor="markdown">Markdown</label>
+        <label htmlFor="markdown">Markdown:</label>{" "}
+        {errors?.markdown && <em>Markdown is required</em>}
         <br />
         <textarea rows={20} name="markdown" />
       </p>
